2033a9a9dffe0a5d1ba66b8d50411eece579da1a
Aakash200 <65803537+Aakash200@users.noreply.github.com>
2020-11-06 01:21:12 +0530

    Create Snake.py

---
+++ Snake.py
@@ -0,0 +1,8 @@
+echo "# mini-Python-Snake-game" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Aakash200/mini-Python-Snake-game.git
+git push -u origin main
+                
